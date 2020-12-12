8202e65e296d8fae9eaecf6f2a322c070943655d
Sol <faisol.che@kdlab.ai>
2020-12-13 02:51:50 +0700

    remove unused component

--- lib/components/index.dart
+++ lib/components/index.dart
@@ -1,3 +1,2 @@
-export 'button.dart';
 export 'container/index.dart';
 export 'typography/index.dart';
--- lib/theme/index.dart
+++ lib/theme/index.dart
@@ -1,3 +1,2 @@
 export 'custom_color_scheme.dart';
 export 'create_material_color.dart';
-export 'text_styles.dart';
